778a9443f67e0553e9720e538eb848ac0c380179
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-26 10:53:08 -0800

    Provide installation method. Move source related attributes under source key

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,8 @@
-default[:passenger][:version]     = "3.0.11"
+default[:passenger][:install_method] = 'source'
+
+default[:passenger][:source][:version]     = "3.0.11"
+default[:passenger][:source][:root_path]   = "#{languages[:ruby][:gems_dir]}/gems/passenger-#{passenger[:version]}"
+default[:passenger][:source][:module_path] = "#{passenger[:source][:root_path]}/ext/apache2/mod_passenger.so"
+default[:passenger][:source][:apache_mpm]  = 'prefork'
 default[:passenger][:max_pool_size] = "6"
-default[:passenger][:root_path]   = "#{languages[:ruby][:gems_dir]}/gems/passenger-#{passenger[:version]}"
-default[:passenger][:module_path] = "#{passenger[:root_path]}/ext/apache2/mod_passenger.so"
-default[:passenger][:apache_mpm]  = 'prefork'
+default[:passenger][:packages] = []
