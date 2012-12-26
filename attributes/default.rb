a28febc1ccdc577ee74d944f17b98a6302edb3b0
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-26 12:48:29 -0800

    Line up versions. Explicit fail on package name undefined

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,10 @@
 default[:passenger][:install_method] = 'source'
 
-default[:passenger][:source][:version]     = "3.0.11"
+default[:passenger][:version]     = "3.0.11"
 default[:passenger][:source][:apache_mpm]  = 'prefork'
 default[:passenger][:root_path]   = "#{languages[:ruby][:gems_dir]}/gems/passenger-#{passenger[:version]}"
 default[:passenger][:module_path] = "#{passenger[:source][:root_path]}/ext/apache2/mod_passenger.so"
 default[:passenger][:max_pool_size] = 6
 default[:passenger][:manage_module_conf] = true
-default[:passenger][:packages] = []
+default[:passenger][:package][:name] = nil
+default[:passenger][:package][:version] = passenger[:version]
