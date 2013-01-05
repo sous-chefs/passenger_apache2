7e068ddecbe2e78e6d6b307b45374f0ea647cb20
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-04 16:16:29 -0800

    Remove superflous attribute set. Add note about module_path attribute set

--- recipes/mod_rails.rb
+++ recipes/mod_rails.rb
@@ -24,7 +24,7 @@
 
 include_recipe "passenger_apache2"
 
-node.default[:passenger][:root_path]   = "#{node[:languages][:ruby][:gems_dir]}/gems/passenger-#{node[:passenger][:version]}"
+# Allows proper default path if root path was overridden
 node.default[:passenger][:module_path] = "#{node[:passenger][:root_path]}/ext/apache2/mod_passenger.so"
 
 if platform?("ubuntu","debian")
