3d5c03ac80a8752178a610bbc48364b002622c25
Kevin Lamontagne <kevin@demarque.com>
2014-11-07 14:55:33 -0500

    Add PassengerMinInstances attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,6 +26,7 @@ default['passenger']['root_path']   = "#{languages['ruby']['gems_dir']}/gems/pas
 default['passenger']['module_path'] = "#{passenger['root_path']}/#{Chef::Recipe::PassengerConfig.build_directory_for_version(passenger['version'])}/apache2/mod_passenger.so"
 default['passenger']['max_pool_size'] = 6
 default['passenger']['max_request_queue_size'] = nil
+default['passenger']['min_instances'] = nil
 default['passenger']['manage_module_conf'] = true
 default['passenger']['package']['name'] = nil
 # set package version to nil, the distro package may not be the same version
