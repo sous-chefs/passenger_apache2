7987d0d7c91f5552515e2508f01e4291e793cea0
Tim Smith <tsmith84@gmail.com>
2015-09-09 20:38:48 -0700

    Opscode -> Chef

--- test/cookbooks/passenger_apache2_test/metadata.rb
+++ test/cookbooks/passenger_apache2_test/metadata.rb
@@ -1,6 +1,6 @@
 name              'passenger_apache2_test'
-maintainer        'Opscode, Inc.'
-maintainer_email  'cookbooks@opscode.com'
+maintainer        'Chef Software, Inc..'
+maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 description       'This cookbook is used with test-kitchen to test the parent, passenger_apache2 cookbok'
 version           '1.0.0'
